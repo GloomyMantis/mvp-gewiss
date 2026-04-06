2ea0ea9fd4dc1718b0911034d77bd602b0fdf6fa
GloomyMantis <gloomymantis@gmail.com>
2026-04-06 13:03:38 +0300

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-import ProjectDetailClient from './ProjectDetailClient'
-
-export async function generateStaticParams() {
-  return [{ id: 'placeholder' }]
-}
-
-export default function AdminProjectDetailPage({ params }: { params: { id: string } }) {
-  return <ProjectDetailClient id={params.id} />
-}
