c4d743c0ee18f4e82a73d18623ebcb7b3d4d753a
GloomyMantis <gloomymantis@gmail.com>
2026-04-06 09:10:41 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,5 +2,11 @@
 const nextConfig = {
   trailingSlash: true,
   images: { unoptimized: true },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+  generateBuildId: async () => 'build',
+  // Skip prerendering pages that use Supabase
+  output: 'standalone',
 }
 module.exports = nextConfig
